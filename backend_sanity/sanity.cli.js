76488f74ba9af2b12fc81e4615384a29a6e0952b
amanpages <151524274+amanpages@users.noreply.github.com>
2024-05-16 13:41:22 +0530

    Add files via upload

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'b1k53e9q',
+    dataset: 'production'
+  }
+})
---
+++ backend_sanity/schemaTypes/index.js
@@ -0,0 +1,9 @@
+import about from "./about";
+import contact from "./contact";
+import experiences from "./experiences";
+import skills from "./skills";
+import testimonials from "./testimonials";
+import work from "./work";
+import workExperience from "./workExperience";
+
+export const schemaTypes = [testimonials, work, skills, contact, about, experiences,workExperience]
